67379ab6cc602129d0ed0972c92523d6ec9a596b
Marcos <falecom@pessoal.aleeas.com>
2025-02-06 11:35:24 -0300

    Commit 06/02/25

---
+++ presentation/src/main/java/com/imbres/despesas/components/SnackBarDisplay_BKP.kt
@@ -0,0 +1,119 @@
+package com.imbres.despesas.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.imbres.despesas.R
+import kotlinx.coroutines.launch
+
+@SuppressLint("CoroutineCreationDuringComposition", "UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SnackBarDisplay_BKP(
+    msg: String, onGoBack: () -> Boolean, error: Boolean,
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    val errorColor = getErrorColor(error)
+
+    Column(
+        modifier = Modifier
+    ) {
+        scope.launch {
+            val result = snackbarHostState.showSnackbar(
+                message = msg,
+                //actionLabel = "Retry",
+                //withDismissAction = true,
+                duration = SnackbarDuration.Short
+            )
+            when (result) {
+                SnackbarResult.Dismissed -> {
+                    // Ação quando o snackbar é dispensado
+                    if (!error) {
+                        //navigateToStart(navController)
+                        onGoBack()
+                    }
+                }
+
+                SnackbarResult.ActionPerformed -> {
+                    // Ação quando o botão "Retry" é pressionado
+                }
+            }
+        }
+    }
+
+    SnackbarHost(
+        hostState = snackbarHostState,
+        snackbar = { snackbarData ->
+            MyCustomSnackbar(snackbarData, Modifier, errorColor)
+        }
+    )
+
+    Column(
+        modifier = Modifier
+    ) {
+        scope.launch {
+            val result = snackbarHostState.showSnackbar(
+                message = msg,
+                //actionLabel = "Retry",
+                //withDismissAction = true,
+                duration = SnackbarDuration.Short
+            )
+            when (result) {
+                SnackbarResult.Dismissed -> {
+                    // Ação quando o snackbar é dispensado
+                    if (!error) {
+                        //navigateToStart(navController)
+                        onGoBack()
+                    }
+                }
+
+                SnackbarResult.ActionPerformed -> {
+                    // Ação quando o botão "Retry" é pressionado
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun getErrorColor(error: Boolean): Color {
+    return if (error) Color.Red else colorResource(id = R.color.blue_500)
+}
+
+@Composable
+private fun MyCustomSnackbar(
+    snackbarData: SnackbarData,
+    modifier: Modifier = Modifier,
+    errorColor: Color,
+) {
+    Snackbar(
+        modifier = modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp),
+        containerColor = errorColor,
+        action = {
+            snackbarData.visuals.actionLabel?.let { actionLabel ->
+                TextButton(onClick = { snackbarData.dismiss() }) {
+                    Text(text = actionLabel, color = Color.White)
+                }
+            }
+        }
+    ) {
+        Text(text = snackbarData.visuals.message, color = Color.White)
+    }
+}
+
--- presentation/src/main/java/com/imbres/despesas/ui/features/sign_in/SignInScreen.kt
+++ presentation/src/main/java/com/imbres/despesas/ui/features/sign_in/SignInScreen.kt
@@ -2,30 +2,51 @@ package com.imbres.despesas.ui.features.sign_in
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.imbres.despesas.R
 import com.imbres.despesas.components.DataStoreManager
 import com.imbres.despesas.components.EmailViewModel
 import com.imbres.despesas.components.PasswordViewModel
 import com.imbres.despesas.components.ValidatingButton
+import com.imbres.despesas.components.ValidatingInputEmail
+import com.imbres.despesas.components.ValidatingInputPassword
 import com.imbres.despesas.components.ViewModelButton
+import com.imbres.despesas.model.UserDetails
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @SuppressLint("CoroutineCreationDuringComposition")
@@ -55,21 +76,21 @@ fun SignInScreen(
         !emailViewModel.emailHasErrors && emailViewModel.email.isNotEmpty() && !passwordViewModel.passwordHasErrors && passwordViewModel.password.isNotEmpty()
     var storeEmail = ""
     var storePassword = ""
-
-    if (userDetails?.email?.isNotEmpty() == true) {
-        storeEmail = userDetails!!.email
-        storePassword = userDetails!!.password
-        emailViewModel.updateEmail((storeEmail))
-        passwordViewModel.updatePassword((storePassword))
-    }
-
     val snackbarHostState = remember { SnackbarHostState() }
+    var errorColor = colorResource(id = R.color.blue_500)
 
     Scaffold(
         modifier = Modifier,
         topBar = {},
         bottomBar = {},
-        snackbarHost = { SnackbarHost(snackbarHostState) },
+        snackbarHost = {
+            SnackbarHost(
+                snackbarHostState,
+                snackbar = { snackbarData ->
+                    MyCustomSnackbar(snackbarData, Modifier, errorColor)
+                }
+            )
+        },
         content = { padding ->
             Column(
                 modifier = Modifier
@@ -79,32 +100,146 @@ fun SignInScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 val scope = rememberCoroutineScope()
+                if (userDetails?.email?.isNotEmpty() == true) {
+                    storeEmail = userDetails!!.email
+                    storePassword = userDetails!!.password
+                    emailViewModel.updateEmail((storeEmail))
+                    passwordViewModel.updatePassword((storePassword))
+                }
+
+                // email
+                ValidatingInputEmail(
+                    email = emailViewModel.email,
+                    updateState = {
+                        if (storeEmail.isNotEmpty()) {
+                            emailViewModel.updateEmail((storeEmail))
+                        } else {
+                            emailViewModel.updateEmail(it)
+                        }
+                        if (it.isEmpty() || it !== userDetails?.email) {
+                            scope.launch {
+                                dataStoreManager.clearDataStore()
+                                emailViewModel.updateEmail(it)
+                            }
+                        }
+                    },
+                    validatorHasErrors = emailViewModel.emailHasErrors,
+                )
+
+                // password
+                ValidatingInputPassword(
+                    password = passwordViewModel.password,
+                    updateState = {
+                        if (storePassword.isNotEmpty()) {
+                            passwordViewModel.updatePassword((storePassword))
+                        } else {
+                            passwordViewModel.updatePassword(it)
+                        }
+                        if (it.isEmpty() || it !== userDetails?.password) {
+                            scope.launch {
+                                dataStoreManager.clearDataStore()
+                                passwordViewModel.updatePassword(it)
+                            }
+                        }
+                    },
+                    validatorHasErrors = passwordViewModel.passwordHasErrors
+                )
 
                 ValidatingButton(onClick, errorButton, "Entrar")
-                if (viewModelButton.signUpUserExists.value) {
-                    scope.launch {
-                        snackbarHostState.showSnackbar(
-                            message = "Usuário localizado.",
-                            actionLabel = "OK",
-                            duration = SnackbarDuration.Short
-                        ).run {
-                            when (this) {
-                                SnackbarResult.Dismissed -> {
-                                    Log.d("SNACKBAR", "Dismissed")
-                                    viewModelButton.signUpUserExists.value = false
-                                }
 
-                                SnackbarResult.ActionPerformed -> {
-                                    Log.d(
-                                        "SNACKBAR",
-                                        "UNDO CLICKED"
-                                    )
-                                    viewModelButton.signUpUserExists.value = false
-                                }
+                var statusMsg = ""
+
+                when {
+                    viewModelButton.signUpUserInvalidCredentials.value -> {
+                        statusMsg = "E-mail e/ou senha inválidos!"
+                        errorColor = Color.Red
+                        viewModelButton.signUpUserInvalidCredentials.value = false
+                    }
+
+                    viewModelButton.signUpFail.value -> {
+                        statusMsg = "Falha ou erro desconhecido."
+                        errorColor = Color.Red
+                        viewModelButton.signUpFail.value = false
+                    }
+
+                    viewModelButton.signUpUserExists.value -> {
+                        statusMsg = "Acessar tela interna - dados ok."
+                        viewModelButton.signUpUserExists.value = false
+                    }
+                }
+
+                if (statusMsg.isNotEmpty()) {
+                    scope.launch {
+                        val result = snackbarHostState.showSnackbar(
+                            message = statusMsg,
+                            actionLabel = "OK",
+                            duration = SnackbarDuration.Short,
+                        )
+                        when (result) {
+                            SnackbarResult.Dismissed -> {
+                                Log.d("SNACKBAR", "Dismissed")
+                                //viewModelButton.signUpUserExists.value = false
+                                statusMsg = ""
+                            }
+
+                            SnackbarResult.ActionPerformed -> {
+                                Log.d(
+                                    "SNACKBAR",
+                                    "UNDO CLICKED"
+                                )
+                                //viewModelButton.signUpUserExists.value = false
+                                statusMsg = ""
                             }
                         }
                     }
+
+                    SnackbarHost(
+                        hostState = snackbarHostState,
+                        snackbar = { snackbarData ->
+                            MyCustomSnackbar(snackbarData, Modifier, errorColor)
+                        }
+                    )
                 }
+
+                /*
+                                if (statusMsg.isNotEmpty()) {
+                                    scope.launch {
+                                        snackbarHostState.showSnackbar(
+                                            message = statusMsg,
+                                            actionLabel = "OK",
+                                            duration = SnackbarDuration.Short,
+                                        ).run {
+                                            when (this) {
+                                                SnackbarResult.Dismissed -> {
+                                                    Log.d("SNACKBAR", "Dismissed")
+                                                    //viewModelButton.signUpUserExists.value = false
+                                                    statusMsg = ""
+                                                }
+
+                                                SnackbarResult.ActionPerformed -> {
+                                                    Log.d(
+                                                        "SNACKBAR",
+                                                        "UNDO CLICKED"
+                                                    )
+                                                    //viewModelButton.signUpUserExists.value = false
+                                                    statusMsg = ""
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                */
+
+                // remember user / lost password
+                ValidatingUserAcess(
+                    userDetails,
+                    scope,
+                    dataStoreManager,
+                    emailViewModel,
+                    passwordViewModel,
+                    onGoToSignUpScreen,
+                    onGoToLostPasswordScreen
+                )
             }
         }
     )
@@ -154,6 +289,120 @@ fun SignInScreen(
             }
         }
     */
+}
+
+@Composable
+private fun ValidatingUserAcess(
+    userDetails: UserDetails?,
+    scope: CoroutineScope,
+    dataStoreManager: DataStoreManager,
+    emailViewModel: EmailViewModel,
+    passwordViewModel: PasswordViewModel,
+    onGoToSignUpScreen: () -> Unit,
+    onGoToLostPasswordScreen: () -> Unit,
+) {
+    var checkedMeuUsuario by remember { mutableStateOf(false) }
+    checkedMeuUsuario = userDetails?.checked ?: false
+
+    Row(
+        Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = checkedMeuUsuario,
+                onCheckedChange = {
+                    scope.launch {
+                        dataStoreManager.saveToDataStore(
+                            UserDetails(
+                                checked = it,
+                                email = if (it) emailViewModel.email else "",
+                                password = if (it) passwordViewModel.password else ""
+                            )
+                        )
+                    }
+                },
+                enabled = emailViewModel.email.isNotEmpty() && passwordViewModel.password.isNotEmpty() && !emailViewModel.emailHasErrors && !passwordViewModel.passwordHasErrors
+            )
+
+            Text(
+                text = "Lembrar meu usuário",
+                modifier = Modifier
+                    .clickable(
+                        enabled = emailViewModel.email.isNotEmpty() && passwordViewModel.password.isNotEmpty() && !emailViewModel.emailHasErrors && !passwordViewModel.passwordHasErrors
+                    ) {
+                        checkedMeuUsuario = !checkedMeuUsuario
+                        scope.launch {
+                            dataStoreManager.saveToDataStore(
+                                UserDetails(
+                                    checked = checkedMeuUsuario,
+                                    email = if (checkedMeuUsuario) emailViewModel.email else "",
+                                    password = if (checkedMeuUsuario) passwordViewModel.password else ""
+                                )
+                            )
+                        }
+                    },
+                fontSize = 14.sp,
+                fontWeight = FontWeight(700),
+                color = colorResource(id = R.color.blue_500),
+            )
+        }
+        Text(
+            text = "Esqueci a senha",
+            modifier = Modifier
+                .padding(end = 20.dp)
+                .clickable {
+                    onGoToLostPasswordScreen()
+                },
+            color = colorResource(id = R.color.blue_500),
+            fontSize = 14.sp,
+            fontWeight = FontWeight(700),
+            textAlign = TextAlign.Start
+        )
+    }
+    Text(
+        text = "Criar uma conta",
+        modifier = Modifier
+            .padding(end = 20.dp)
+            .clickable {
+                onGoToSignUpScreen()
+            },
+        color = colorResource(id = R.color.blue_500),
+        fontSize = 14.sp,
+        fontWeight = FontWeight(700),
+        textAlign = TextAlign.Start
+    )
+
+}
+
+@Composable
+private fun getErrorColor(error: Boolean): Color {
+    return if (error) Color.Red else colorResource(id = R.color.blue_500)
+}
+
+@Composable
+private fun MyCustomSnackbar(
+    snackbarData: SnackbarData,
+    modifier: Modifier = Modifier,
+    errorColor: Color,
+) {
+    Snackbar(
+        modifier = modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp),
+        containerColor = errorColor,
+        action = {
+            snackbarData.visuals.actionLabel?.let { actionLabel ->
+                TextButton(onClick = { snackbarData.dismiss() }) {
+                    Text(text = actionLabel, color = Color.White)
+                }
+            }
+        }
+    ) {
+        Text(text = snackbarData.visuals.message, color = Color.White)
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
